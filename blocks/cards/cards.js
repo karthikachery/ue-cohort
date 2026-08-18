487765f0e48429f289379449157da4f6b38e1262
VMuthulakshmi <53695045+VMuthulakshmi@users.noreply.github.com>
2026-08-18 17:01:22 +0530

    Updated the card styles

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -7,6 +7,7 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
+    console.log("li ",li)
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
